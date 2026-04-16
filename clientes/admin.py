6df2f6705909194abd099277279eac7d4d5a9db8
Eduardo Marchi <edu.3e@hotmail.com>
2026-04-16 20:42:09 -0300

    Primeiro commit

---
+++ clientes/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Cliente, Servico
+
+# Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Servico)
---
+++ clientes/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class Cliente(models.Model):
+    id = models.AutoField(primary_key=True) 
+    nome = models.CharField(max_length=100)
+    documento = models.CharField(max_length=20, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    telefone = models.CharField(max_length=20, null=True, blank=True)
+    endereco = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Servico(models.Model):
+    STATUS_SERVICO = [
+        ('P', 'Pendente'),
+        ('C', 'Concluído'),
+        ('X', 'Cancelado'),
+    ]
+    
+    id = models.AutoField(primary_key=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    descricao = models.TextField()
+    data = models.DateField()
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=1, choices=STATUS_SERVICO, default='P')
+
+    def __str__(self):
+        return f'{self.cliente} - {self.data}'
