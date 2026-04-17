3598cb616032781ea83d9f3b7c4ecb9ac1ebd5e1
Eduardo Marchi <edu.3e@hotmail.com>
2026-04-17 18:23:32 -0300

    feat(clientes): Adicionada tela de criação de cliente

--- clientes/urls.py
+++ clientes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('clientes/', views.ListaClientesView.as_view(), name='lista-clientes'),
+    path('clientes/novo/', views.CriarClienteView.as_view(), name='novo-cliente'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Cliente
 
 # Create your views here.
@@ -7,3 +7,10 @@ class ListaClientesView(ListView):
     model = Cliente
     template_name = 'lista_clientes.html'
     context_object_name = 'clientes'
+
+
+class CriarClienteView(CreateView):
+    model = Cliente
+    template_name = 'criar_cliente.html'
+    fields = '__all__'
+    success_url = '/clientes/'
