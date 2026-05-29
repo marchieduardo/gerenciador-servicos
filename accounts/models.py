e0cf75931409d848b33a1b04e552e745b0143097
Eduardo Marchi <edu.3e@hotmail.com>
2026-05-29 19:50:26 -0300

    feat(accounts): usar email como campo de login no model de usuario

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,17 @@ from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
     telefone = models.CharField(max_length=20, null=True, blank=True)
+    email = models.EmailField(unique=True) # Redefinindo o campo padrão email para torná-lo unique
+
+    USERNAME_FIELD = 'email' # Dizendo ao Django que o campo de login será o email
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.username
+
+    # Sobreescrevendo método padrão para retornar o username caso o campo first_name estiver vazio.
+    # Obs: Por padrão o fallback usava get_username(), que retornava o email pois definimos como USERNAME_FIELD.
+    def get_short_name(self):
+        if self.first_name:
+            return self.first_name
+        return self.username # fallback
